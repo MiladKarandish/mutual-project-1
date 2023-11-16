eb6e33d21153ab66d9182e8dc0f2fc541e148965
Milad Karandish <80484107+MiladKarandish@users.noreply.github.com>
2023-11-16 14:28:41 +0330

    fetcher sample added

--- src/controllers/fetcher.ts
+++ src/controllers/fetcher.ts
@@ -1,18 +1,11 @@
-const fetcher = async (url: string, options: any = {}) => {
-    const baseUrl = "http://localhost:1337";
-    const opt = { ...options };
-    if (!opt.headers) opt.headers = {};
-  
-    opt.headers.Authorization =
-      "Bearer";
-  
-      fetch('https://example.com?' + new URLSearchParams({
-    foo: 'value',
-    bar: 2,
-}))
+const fetcher = async (options: any = {}) => {
+  const baseUrl = `http://www.omdbapi.com/?apikey=ac0ab82e&t=titanic&`;
+  const opt = { ...options };
 
-    return await fetch(`${baseUrl}/${url}`, opt);
-  };
-  
-  export default fetcher;
-  
+  const parameters = new URLSearchParams();
+  parameters.set("t", "titanic");
+
+  return await fetch(baseUrl, opt);
+};
+
+export default fetcher;
